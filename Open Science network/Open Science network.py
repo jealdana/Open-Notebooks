796a1b3dff8bf096fa6a83ea49fca651407f974d
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2017-09-24 15:19:07 -0400

    Upload Topics
    Data analysis
    Social Network Analysis
    Python
    etc.

---
+++ Open Science network/Open Science network.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In this notebook, I'm going to present you the graphicar representation of people related with open science over time. For this purpose, I have created a graph with the authors 
+
+# In[ ]:
+
+
+
+
---
+++ Regression/Regressions tutorial.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# # Regressions
+# ## Checking assumptions of regressions
+# [Chong Ho Yu, Ph.D.](http://www.creative-wisdom.com/computer/sas/regression_assumption.html)
+# # How to handle zeros in log transformations
+# 
+# [Ref1](http://onlinelibrary.wiley.com/doi/10.1111/j.2041-210X.2010.00021.x/full)
