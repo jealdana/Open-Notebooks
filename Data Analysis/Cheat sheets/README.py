54548b736b287e16730aa0b894ead21e4fb4569e
Aldana-Sigona, Jesus-Enrique <jealdana@users.noreply.github.com>
2017-10-05 20:50:29 -0400

    Dana analysis

---
+++ Data Analysis/Cheat sheets/README.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# ## Reference:
+# [More Cheat sheets](https://www.kdnuggets.com/2017/09/essential-data-science-machine-learning-deep-learning-cheat-sheets.html)
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
